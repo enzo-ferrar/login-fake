ccd47824629836264d8a9de3f72b3c513ec4320b
enzo-ferrar <enzo_ferrari@estudante.sesisenai.org.br>
2024-09-30 15:50:22 -0300

    upload
    Fazer upload dos arquivos

---
+++ js.js
@@ -0,0 +1,32 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+/*document.getElementById('PagFalsa').addEventListener('submit', function(){
+    event.preventDefault();//impede o envio do formulario
+    
+    const Numero_Conta = document.formularioIMC.Numero_Conta.value;
+    var CPF = document.formularioIMC.CPF.value;
+    const Senha = document.formularioIMC.Senha.value;
+    const Altura = document.formularioIMC.Altura.value;
+    var situacao;
+    
+
+document.getElementById('mensagemSaida').innerHTML =
+        "Obrigado pelos seus dados aguarde a resposta";
+        
+     
+//mostra a saida
+document.getElementById('mensagem').style.display = 'block';
+
+
+});*/
+
+document.getElementById('PagFalsa').addEventListener(
+        'submit', function () {
+            event.preventDefault();
+           
+            
+            alert("Obrigado pelos seus dados aguarde a resposta");
+});
